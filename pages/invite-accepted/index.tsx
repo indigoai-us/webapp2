346235b7fc4887c8f62cbfbd3b370910f71c693d
johnsonfamily1234 <therealstefan@gmail.com>
2023-12-24 19:26:46 -0800

    finishing signin flows

--- pages/invite-accepted/index.tsx
+++ pages/invite-accepted/index.tsx
@@ -27,7 +27,7 @@ export default function InviteAccepted() {
   return (
     <main className="flex flex-col h-screen justify-center items-center">
       <h1 className="text-3xl font-bold text-center text-white-700 flex justify-center items-center relative right-2 mb-8">
-        <Image src="/512x512.png" width="40" alt="Indigo" />
+        <Image src="/512x512.png" width="40" height="40" alt="Indigo" />
         <div className="text-5xl ml-3 relative bottom-1">Indigo</div>
       </h1>
     </main>
